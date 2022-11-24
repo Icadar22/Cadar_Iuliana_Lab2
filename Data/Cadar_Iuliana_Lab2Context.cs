fcdf58b8a46b54e59f7a26748b20b6ff167752d7
Icadar22 <cadariuliana2000@yahoo.com>
2022-11-24 18:27:09 +0200

    LABORATOR 5

--- Data/Cadar_Iuliana_Lab2Context.cs
+++ Data/Cadar_Iuliana_Lab2Context.cs
@@ -21,5 +21,9 @@ namespace Cadar_Iuliana_Lab2.Data
         public DbSet<Cadar_Iuliana_Lab2.Models.Author> Author { get; set; }
 
         public DbSet<Cadar_Iuliana_Lab2.Models.Category> Category { get; set; }
+
+        public DbSet<Cadar_Iuliana_Lab2.Models.Borrowing> Borrowing { get; set; }
+
+        public DbSet<Cadar_Iuliana_Lab2.Models.Member> Member { get; set; }
     }
 }
---
+++ Pages/Members/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Cadar_Iuliana_Lab2.Data;
+using Cadar_Iuliana_Lab2.Models;
+
+namespace Cadar_Iuliana_Lab2.Pages.Members
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Cadar_Iuliana_Lab2.Data.Cadar_Iuliana_Lab2Context _context;
+
+        public IndexModel(Cadar_Iuliana_Lab2.Data.Cadar_Iuliana_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Member> Member { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Member != null)
+            {
+                Member = await _context.Member.ToListAsync();
+            }
+        }
+    }
+}
